5e880e3dfe13712674cffe1d0fe667139e241358
heyyouknowme <ntntran23@clc.fitus.edu.vn>
2024-06-28 10:51:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # Cấu hình chung cho cả client và server
 SERVER_IP = socket.gethostbyname(socket.gethostname())  # Địa chỉ IP của server (localhost trong ví dụ này)
 SERVER_PORT = 6666      # Cổng kết nối
+SERVER_ADDR =  (IP, PORT)
 CHUNK_SIZE = 1024 * 1024  # Kích thước chunk: 1MB
 TIMEOUT = 1             # Thời gian timeout (giây)
+FORMAT = "utf-8"
