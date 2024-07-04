7530f5f32921d2f6cf2df84e1759fbfff1782432
heyyouknowme <ntntran23@clc.fitus.edu.vn>
2024-07-04 23:00:58 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -54,68 +54,78 @@ class Server:
 
     def calculate_checksum(self, data):
         return hashlib.sha256(data).hexdigest()
-
+    
     def send_chunk(self, client_socket, chunk_num, chunk_data):
         retries = 3  # Số lần thử lại tối đa
         while retries > 0:
             try:
                 checksum = self.calculate_checksum(chunk_data)
-                data_with_checksum = checksum.encode() + b'::' + chunk_data
+                data_with_checksum_chunk_num = checksum.encode() + b'::' + str(chunk_num).encode() +  b'::' + chunk_data
                 with self.lock:
-                    self.send_data(client_socket, data_with_checksum)
+                    self.send_data(client_socket, data_with_checksum_chunk_num)
                     ack = self.receive_data(client_socket)
                     if ack.decode() == "ACK":
-                        logging.info(f"Chunk {chunk_num} sent to client successfully.")
+                        logging.info(f"Chunk {chunk_num} uploaded successfully.")
                         return True  # Thành công, không cần thử lại nữa
             except Exception as e:
-                logging.error(f"Error sending chunk {chunk_num}: {e}")
+                logging.error(f"Error uploading chunk {chunk_num}: {e}")
                 retries -= 1
-        logging.error(f"Failed to send chunk {chunk_num} after {retries} retries.")
+        logging.error(f"Failed to upload chunk {chunk_num} after {retries} retries.")
         return False
 
-    def receive_chunk(self, client_socket, chunk_num):
+    def receive_chunk(self, client_socket):
         retries = 3  # Số lần thử lại tối đa
         while retries > 0:
             try:
                 data_with_checksum = self.receive_data(client_socket)
                 if data_with_checksum:
-                    received_checksum, chunk_data = data_with_checksum.split(b'::', 1)
+                    received_checksum, chunk_num, chunk_data = data_with_checksum.split(b'::', 2)
                     calculated_checksum = self.calculate_checksum(chunk_data)
                     if received_checksum.decode() == calculated_checksum:
                         self.send_data(client_socket, b"ACK")
-                        logging.info(f"Chunk {chunk_num} received successfully.")
-                        return chunk_data
+                        logging.info(f"Chunk {chunk_num.decode()} received successfully.")
+                        return chunk_num, chunk_data
             except Exception as e:
-                logging.error(f"Error receiving chunk {chunk_num}: {e}")
+                logging.error(f"Error receiving chunk: {e}")
                 retries -= 1
-        logging.error(f"Failed to receive chunk {chunk_num} after {retries} retries.")
+        logging.error(f"Failed to receive chunk after {retries} retries.")
         return None
 
-    def receive_file(self, client_socket, filename, filesize):
+    def receive_file(self, client_socket, filename):
+
+        file_chunks = []
+        while True:
+            chunk_num, chunk_data = self.receive_chunk(client_socket)
+            if not chunk_data:
+                break
+            file_chunks.append((int(chunk_num.decode()), chunk_data))
+
         with open(filename, 'wb') as f:
-            chunk_num = 0
-            while filesize > 0:
-                chunk_data = self.receive_chunk(client_socket, chunk_num)
-                if chunk_data:
-                    f.write(chunk_data)
-                    filesize -= len(chunk_data)
-                    chunk_num += 1
-                else:
-                    break
+            for chunk_num, chunk_data in sorted(file_chunks):
+                f.write(chunk_data)
         
         logging.info(f"File {filename} received successfully.")
 
     def send_file(self, client_socket, filename):
         try:
+            threads = []
             with open(filename, 'rb') as f:
                 chunk_num = 0
                 while True:
                     chunk_data = f.read(BUFFER_SIZE)
                     if not chunk_data:
+                        thread = threading.Thread(target=self.send_chunk, args=(client_socket, chunk_num, "".encode()))
+                        thread.start()
+                        threads.append(thread)
                         break
                     thread = threading.Thread(target=self.send_chunk, args=(client_socket, chunk_num, chunk_data))
                     thread.start()
+                    threads.append(thread)
                     chunk_num += 1
+
+            for thread in threads:
+                thread.join()
+
             logging.info(f"File {filename} sent successfully.")
         except FileNotFoundError:
             logging.error(f"File {filename} not found.")
@@ -130,8 +140,7 @@ class Server:
 
                 if action == b'u':
                     filename = self.receive_data(client_socket).decode()
-                    filesize = int(self.receive_data(client_socket).decode())
-                    self.receive_file(client_socket, filename, filesize)
+                    self.receive_file(client_socket, filename)
                 elif action == b'd':
                     filename = self.receive_data(client_socket).decode()
                     self.send_file(client_socket, filename)
